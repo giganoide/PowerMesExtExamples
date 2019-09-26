cddbfaf816249c7e0810dbbdd8da51c070b11c30
Luca Bonini <l.bonini@teamsystem.com>
2019-09-26 11:50:41 +0200

    Add Foundation project

---
+++ 005_Foundations/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Foundations
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var classList = new List<ICalculation> { new Moltiplication(), new Addition() };
+            foreach (var c in classList)
+            {
+                Console.WriteLine($"{c.Description}: {c.Calc(2, 4)}");
+            }
+            Console.ReadKey();
+        }
+    }
+
+    public interface ICalculation
+    {
+        string Description { get; }
+        decimal Calc(decimal a, decimal b);
+    }
+
+    public class Moltiplication : ICalculation
+    {
+        public string Description { get { return "Moltiplication of two numbers"; } }
+
+        public decimal Calc(decimal a, decimal b)
+        {
+            return a * b;
+        }
+    }
+
+    public class Addition : ICalculation
+    {
+        public string Description { get { return "Addition of two numbers"; } }
+
+        public decimal Calc(decimal a, decimal b)
+        {
+            return a + b;
+        }
+    }
+}
