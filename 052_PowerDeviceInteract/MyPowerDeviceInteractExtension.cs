7321f6029d92298a0d41d7f49fc28f3185a0ba9e
Luca Bonini <l.bonini@teamsystem.com>
2019-09-26 14:32:27 +0200

    Update readme and add PowerDeviceInteract project

---
+++ 052_PowerDeviceInteract/MyPowerDeviceInteractExtension.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Atys.PowerMES.Extensibility;
+using Atys.PowerMES.Foundation;
+using Atys.PowerMES.Support;
+
+namespace TeamSystem.Customizations
+{
+    [ExtensionData("MyPowerDeviceInteractExtension", "Put here an extension description.", "1.0",
+        Author = "TeamSystem", EditorCompany = "TeamSystem")]
+    public class MyPowerDeviceInteractExtension : IMesExtension
+    {
+        private const string LOGSOURCE = @"MYPOWERDEVICEINTERACTEXTENSION";
+
+        private IMesManager _MesManager = null;
+        private IMesAppLogger _Logger = null;
+
+        #region IMesExtension members
+
+        /// <summary>
+        /// Inizializzazione estensione e collegamento all'oggetto principale PowerMES
+        /// (eseguito al caricamento in memoria dell'estensione)
+        /// </summary>
+        /// <param name="mesManager">Riferimento all'oggetto principale PowerMES</param>
+        public void Initialize(IMesManager mesManager)
+        {
+#if DEBUG
+            Debugger.Launch();
+#endif
+            this._MesManager = mesManager;
+            this._Logger = this._MesManager.ApplicationMainLogger;
+
+        }
+
+        /// <summary>Esegue/avvia l'estensione</summary>
+        public void Run()
+        {
+            this._Logger.WriteMessage(MessageLevel.Diagnostics, true, LOGSOURCE,
+                "Estensione creata!");
+            
+        }
+
+        /// <summary>
+        /// Deve contenere il codice di cleanup da eseguire prima della disattivazione
+        /// dell'estensione o comunque alla chiusura di PowerMES
+        /// </summary>
+        public void Shutdown()
+        {
+            
+        }
+
+        #endregion
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # PowerMesExtExamples
 Esempi di codice per creare extension PowerMES
+
+## Extensions PowerMES
+* Progetto di tipo class library
+* Estensione DLL: «.MesExt.dll»
+* Una sola extension per DLL
+* Referenziare le librerie: 
+*  Atys.PowerMES.Contracts
+    *  Atys.PowerMES.Contracts
+    * Atys.PowerMES.Foundation
+    * Opzionale Atys.PowerMES.Support
+* Deve essere creata una classe pubblica
+    * Deve implementare l’interfaccia «IMesExtension»
+    * Deve essere decorata con l’attributo «ExtensionData»
+    * Aggiungere using: Atys.PowerMES.Extensibility, Atys.PowerMES.Foundation, Atys.PowerMES.Support
+
+
+La DLL deve essere posizionata in una sotto-cartella del percorso di installazione «C:\Program Files (x86)\Atys\PowerMES\Extensions», una DLL per ogni sotto-cartella
+
+## Esempi
+1. 005_Foundations: Premesse di base 
+2. 010_Empty: Struttura di base per la creazione di un extension
+3. 015_Events: Utilizzo degli eventi di sistema
+4. 020_Store: Memorizzazione di informazioni
+5. 025_ExternalCommands: utilizzo dei comandi esterni
+6. 030_ScheduledActivity: Pianificazione di un'attività schedulata
+7. 035_Attributes: utilizzo degli attributi per configurare l'extension
+8. 040_CancelEvent: utilizzo degli eventi cancellabili
+9. 045_FileSystemWatcher: parsing di un file di una cartella monitorata
+10. 050_DbMonitor: utilizzo un db esterno per integrare informazioni
+11. 052_PowerDeviceInteract: interagire con PowerDevice
+12. 055_MesInteract: interagire con MES
+13. 060_NicimIteract: interagire con Nicim
